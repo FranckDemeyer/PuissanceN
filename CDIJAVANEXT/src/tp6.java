058cd2186424864886191d920c3c6e6d7791dc41
Fr@nck D. <franck.demeyer@free.fr>
2017-05-16 10:19:32 +0200

    modif swap fonctionnelle

--- CDIJAVANEXT/src/tp6.java
+++ CDIJAVANEXT/src/tp6.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicInteger;
 public class tp6 {
 	public static void main(String[] args) {
-		Integer a, b;
+		AtomicInteger a, b;
 		Scanner clavier = new Scanner(System.in);
 		System.out.println("entrez un nombre entier quelconque :");
-		a = new Integer(clavier.nextInt());
+		a = new AtomicInteger(clavier.nextInt());
 		System.out.println("entrez un deuxième nombre entier quelconque :");
-		b = new Integer(clavier.nextInt());
+		b = new AtomicInteger(clavier.nextInt());
 		clavier.close();
 		System.out.println("a : " + a + ", b : " + b);
 		swap(a, b);
 		System.out.println("a : " + a + ", b : " + b);
 	}
-	static void swap(Integer a, Integer b) {
-		System.out.println("swap a before > " + a + ", b before > " + b);
-		Integer temp;
-		temp = a;
-		a = b;
-		b = temp;
-		System.out.println("swap a after > " + a + ", b after > " + b);
+	public static void swap(AtomicInteger a,AtomicInteger b) {
+		a.set(b.getAndSet(a.get()));;
 	}
 }
