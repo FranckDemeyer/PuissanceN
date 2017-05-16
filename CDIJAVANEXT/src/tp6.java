53a5b362a25d5c2faf0bb63e4020b1da8c0a6c7d
Fr@nck D. <franck.demeyer@free.fr>
2017-05-16 10:35:30 +0200

    version avec permutation par passage de tableau

--- CDIJAVANEXT/src/tp6.java
+++ CDIJAVANEXT/src/tp6.java
@@ -3,17 +3,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class tp6 {
 	public static void main(String[] args) {
 		AtomicInteger a, b;
+		int [] tabInt;
+		tabInt = new int[2];
 		Scanner clavier = new Scanner(System.in);
 		System.out.println("entrez un nombre entier quelconque :");
 		a = new AtomicInteger(clavier.nextInt());
+		tabInt[0] = a.get();
 		System.out.println("entrez un deuxième nombre entier quelconque :");
 		b = new AtomicInteger(clavier.nextInt());
+		tabInt[1] = b.get();
 		clavier.close();
+		System.out.println("en mode Atomic Integer :");
 		System.out.println("a : " + a + ", b : " + b);
 		swap(a, b);
 		System.out.println("a : " + a + ", b : " + b);
+		System.out.println("en mode tableau :");
+		System.out.println("a : " + tabInt[0] + ", b : " + tabInt[1]);
+		swapTab(tabInt);
+		System.out.println("a : " + tabInt[0] + ", b : " + tabInt[1]);
 	}
 	public static void swap(AtomicInteger a,AtomicInteger b) {
 		a.set(b.getAndSet(a.get()));;
+	}
+	public static void swapTab(int [] tabInt){
+		int temp;
+		temp = tabInt[0];
+		tabInt[0] = tabInt[1];
+		tabInt[1] = temp;
 	}
 }
